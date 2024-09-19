348b1d0ffd409019aaa27756cd85e812e51fd62e
tomashinM <tomashin.m@gmail.com>
2024-09-19 17:30:28 +0500

    fix

--- telegrambot/config.py
+++ telegrambot/config.py
@@ -7,5 +7,5 @@ from redis import Redis
 from telebot import TeleBot
 
 redis_client = Redis.from_url("redis://redis:6379/2")
-api = API(ApiClient(Configuration(host="http://django:8000")))
+api = API(ApiClient(Configuration(host="http://django:8000/api")))
 bot = TeleBot(os.environ.get("TELEGRAM_TOKEN"))
